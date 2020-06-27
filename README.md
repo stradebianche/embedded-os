2aceb812d4bd4bf6abc019793318aacd4c8ff24a
bfl <flakbartlomiej@gmail.com>
2020-06-27 11:32:36 +0200

    Added linker script and main configuration file.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # v8bsp
 Boot code for ARMv8
+
+Just to learn more about ARMv8.
---
+++ config.h
@@ -0,0 +1,14 @@
+/*
+ * v8bsp
+ *
+ * Configuration file for ARMv8
+ */
+ 
+ #ifndef _CONFIGURATION_H_
+ #define _CONFIGURATION_H_
+ 
+ #define CONFIG_MEMORY_BASE			0x00000000
+ #define CONFIG_MEMORY_SIZE			0x100000
+ 
+ #endif
+ 
