eaaee3f5fa6bda60ea0ecfe54f3aebc000fd36aa
stradebianche <flakbartlomiej@gmail.com>
2021-04-02 09:15:28 +0200

    Introducing .gitignore file, refactoring run.sh

--- qemu/run.sh
+++ qemu/run.sh
@@ -1,1 +1,4 @@
-qemu-system-aarch64 -M virt -cpu cortex-a53 -nographic -kernel ../main.elf
+#!/bin/bash
+echo 'Launching image.elf on qemu-system-aarch64 (Press Ctr+a, x to terminate):'
+qemu-system-aarch64 -M virt -cpu cortex-a53 -nographic -kernel image.elf
+echo 'Exit'
